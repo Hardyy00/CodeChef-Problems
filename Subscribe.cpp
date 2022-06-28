2852490533d55d6e5d594f726340efd47f470cbc
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-06-28 11:59:40 +0530

    Add files via upload

---
+++ Subscribe.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n,x;
+        cin >> n >> x;
+        if(n%6==0)
+            cout << (n/6)*x << endl;
+        else
+            cout << (n/6)*x + x << endl;
+    }
+}
