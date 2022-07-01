cfb6e3352e1124f48f3f6eb99ec982f86bc981e1
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-07-01 22:01:19 +0530

    Add files via upload

---
+++ SAVWATER.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int h,x,y,c;
+        cin >> h >> x >> y >>c;
+        if(h*(x+(y/2)) <=c)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+    return 0;
+}
