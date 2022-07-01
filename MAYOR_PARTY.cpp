6e8de6c473a1a93dcdec21d69d976fee67e23c69
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-07-01 22:06:30 +0530

    Add files via upload

---
+++ MAYOR_PARTY.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int p1,p2,p3;
+        cin >> p1>>p2>>p3;
+        if(p1+p3>=p2)
+            cout << p1+p3 << endl;
+        else
+            cout << p2 << endl;
+        
+    }
+    return 0;
+}
