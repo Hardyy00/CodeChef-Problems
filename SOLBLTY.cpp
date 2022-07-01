8f24613da09dbf3d07315cdab5429f0b5800258a
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-07-01 22:00:37 +0530

    Add files via upload

---
+++ SOLBLTY.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int x,a,b;
+        cin >> x >> a >> b;
+        cout << (a+b*(100-x))*10 << endl;
+    }
+    return 0;
+}
