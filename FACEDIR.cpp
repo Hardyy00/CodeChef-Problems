eb4d6cae5045ebaa8f390ab77c8a2edc417499f4
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-07-02 22:59:53 +0530

    Add files via upload

---
+++ FACEDIR.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int x;
+        cin >> x;
+        switch(x%4)
+        {
+            case 0 :
+                cout << "North" << endl;
+                break;
+            case 1 :
+                cout << "East" << endl;
+                break;
+            case 2 :
+                cout << "South" << endl;
+                break;
+            case 3 :
+                cout << "West" << endl;
+        }
+    }
+    return 0;
+}
