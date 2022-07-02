88cd9580bcac48820b70eebfd68f3037a3a5d7a6
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-07-02 22:58:00 +0530

    Add files via upload

---
+++ CHEFRUN.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int x1,x2,x3,v1,v2;
+        cin >> x1 >> x2 >> x3 >> v1 >> v2;
+        float t1=abs(x3-x1)/float(v1) ,t2 = abs(x3-x2)/float(v2);
+        if(t1<t2)
+            cout << "Chef" << endl;
+        else if(t2<t1)
+            cout << "Kefa" << endl;
+        else
+            cout << "Draw" << endl;
+    }
+    return 0;
+}
