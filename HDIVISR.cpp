579ae3ba8d7161f96b524d0742b17ea8ceb6b93c
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-07-03 22:59:59 +0530

    Add files via upload

---
+++ HDIVISR.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   int n;
+   cin >>n;
+   int max;
+   for(int i=1;i<=10;i++)
+        if(n%i==0)
+            max=i;
+        
+    cout << max << endl;
+    return 0;
+}
