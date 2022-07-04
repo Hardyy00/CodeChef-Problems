4b979a3f48d2c46ac6eecb3bb9ed9c5a2f1d0373
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-07-04 23:08:01 +0530

    Add files via upload

---
+++ COCONUT.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int x1,x2,X1,X2;
+        cin >> x1 >> x2 >> X1 >> X2 ;
+        cout << X1/x1 + X2/x2 << endl;
+        
+       
+    }
+    return 0;
+}
