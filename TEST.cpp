7da913e793d4922d36f7c60a2c4597544c1aea04
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-07-16 22:44:54 +0530

    Add files via upload

---
+++ TEST.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    while(true){
+        int num;
+        cin >> num;
+        
+        if(num == 42){
+            break;
+        }
+        
+        cout << num << endl;
+    }
+    
+    return 0;
+}
