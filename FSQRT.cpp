9cebd8574d68452c9c6e7d98f855ea54052ae5aa
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-07-17 23:09:23 +0530

    Add files via upload

---
+++ FSQRT.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int num;
+        cin >> num;
+        
+        cout << (int)sqrt(num) << endl;
+    }
+    
+    return 0;
+}
