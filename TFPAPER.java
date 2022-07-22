29cd55c5b7da6fb7027b52530bdea56d20819b88
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-07-22 23:08:23 +0530

    Add files via upload

---
+++ TFPAPER.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+
+class Codechef {
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+
+        int t = cin.nextInt();
+
+        while (t > 0) {
+
+            int n = cin.nextInt();
+            int k = cin.nextInt();
+
+            System.out.println(n - k);
+
+            t--;
+
+        }
+        cin.close();
+    }
+}
