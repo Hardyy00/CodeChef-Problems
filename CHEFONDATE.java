a44c5fac09db8036c2ef64410fd7befe65bcced7
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-07-24 23:10:32 +0530

    Add files via upload

---
+++ CHEFONDATE.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class CodeChef {
+
+    public static void main(String[] args) {
+
+        Scanner cin = new Scanner(System.in);
+
+        int t = cin.nextInt();
+
+        while (t > 0) {
+
+            int x = cin.nextInt();
+            int y = cin.nextInt();
+
+            String ans = x >= y ? "YES" : "NO";
+
+            System.out.println(ans);
+            t--;
+        }
+
+        cin.close();
+    }
+}
