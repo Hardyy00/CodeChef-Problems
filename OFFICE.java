a4f191d48605c18e39eef1f6243bbd5f6f251138
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-08-05 23:23:12 +0530

    Add files via upload

---
+++ OFFICE.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class CodeChef{
+    
+    public static void main(String[] args){
+        
+        Scanner cin = new Scanner(System.in);
+        
+        int t = cin.nextInt();
+        
+        while(t>0){
+            
+            int x = cin.nextInt();
+            int y = cin.nextInt();
+            
+            int hrs = (4*x) + y;
+            
+            System.out.println(hrs);
+            
+            t--;
+        }
+        cin.close();
+        
+    }
+}
