de81956f4ebfff7fded2b8731e2144f41b4ff143
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-09-18 23:04:39 +0530

    Add files via upload

---
+++ MUFFINS3.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef {
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner cin = new Scanner(System.in);
+        int t = cin.nextInt();
+
+        while (t-- > 0) {
+
+            int num = cin.nextInt();
+
+            System.out.println(num / 2 + 1);
+
+        }
+
+        cin.close();
+    }
+}
