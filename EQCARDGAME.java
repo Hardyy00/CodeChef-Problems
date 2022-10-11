2b87061547199ece014c685e7239584cacf1e98d
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-10-11 22:39:00 +0530

    Add files via upload

---
+++ EQCARDGAME.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Codechef {
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner cin = new Scanner(System.in);
+        int t = cin.nextInt();
+
+        while (t-- > 0) {
+
+            int k = cin.nextInt();
+
+            System.out.println(52 % k);
+        }
+
+        cin.close();
+    }
+}
